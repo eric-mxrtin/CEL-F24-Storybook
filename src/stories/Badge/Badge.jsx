57f7376304713e7148f6138eda30143d563a3ef8
Eric M <ern.martin12@gmail.com>
2024-11-04 16:40:52 -0500

    added size variants

--- src/stories/Badge/Badge.jsx
+++ src/stories/Badge/Badge.jsx
@@ -1,19 +1,111 @@
-export const Badge = ( { type, color, label } ) => {
-
- if(type == "outline") {
+export const Badge = ({ type, color, label, size, iconLeft, iconRight }) => {
+    if (size == "small") {
+      if (type == "outline") {
+        return (
+          <span
+            className={`inline-flex items-center gap-x-1.5 py-px px-2 rounded-md text-xs font-medium border border-${color}-500 text-${color}-500`}
+          >
+            {iconLeft}
+            {label}
+            {iconRight}
+          </span>
+        );
+      }
+  
+      if (type == "soft") {
+        return (
+          <span
+            className={`inline-flex items-center gap-x-1.5 py-px px-2 rounded-md text-xs font-medium bg-${color}-50 text-${color}-800 dark:bg-${color}-800/30 dark:text-${color}-500`}
+          >
+            {iconLeft}
+            {label}
+            {iconRight}
+          </span>
+        );
+      }
+      return (
+        <>
+          <span
+            className={`inline-flex items-center gap-x-1.5 py-px px-2 rounded-md text-xs font-medium bg-${color}-500 text-white`}
+          >
+            {iconLeft}
+            {label}
+            {iconRight}
+          </span>
+        </>
+      );
+    }
+  
+    if (size == "large") {
+      if (type == "outline") {
+        return (
+          <span
+            className={`inline-flex items-center gap-x-1.5 py-[11px] px-[10px] rounded-md text-xs font-medium border border-${color}-500 text-${color}-500`}
+          >
+            {iconLeft}
+            {label}
+            {iconRight}
+          </span>
+        );
+      }
+  
+      if (type == "soft") {
+        return (
+          <span
+            className={`inline-flex  items-center gap-x-1.5 py-[11px] px-[10px] rounded-md text-xs font-medium bg-${color}-50 text-${color}-800 dark:bg-${color}-800/30 dark:text-${color}-500`}
+          >
+            {iconLeft}
+            {label}
+            {iconRight}
+          </span>
+        );
+      }
+      return (
+        <>
+          <span
+            className={`inline-flex items-center gap-x-1.5 py-[11px] px-[10px] rounded-md text-xs font-medium bg-${color}-500 text-white`}
+          >
+            {iconLeft}
+            {label}
+            {iconRight}
+          </span>
+        </>
+      );
+    }
+  
+    if (type == "outline") {
+      return (
+        <span
+          className={`inline-flex items-center gap-x-1.5 py-1.5 px-1.5 rounded-md text-xs font-medium border border-${color}-500 text-${color}-500`}
+        >
+          {iconLeft}
+          {label}
+          {iconRight}
+        </span>
+      );
+    }
+  
+    if (type == "soft") {
+      return (
+        <span
+          className={`inline-flex items-center gap-x-1.5 py-1.5 px-1.5 rounded-md text-xs font-medium bg-${color}-50 text-${color}-800 dark:bg-${color}-800/30 dark:text-${color}-500`}
+        >
+          {iconLeft}
+          {label}
+          {iconRight}
+        </span>
+      );
+    }
     return (
-        <span className={`inline-flex items-center gap-x-1.5 py-1.5 px-2.5 rounded-md text-xs font-medium border border-${color}-500 text-${color}-500`}>{label}</span>
-    )
- }  
- if (type == "soft") {
-    return (
-        <span className={`inline-flex items-center gap-x-1.5 py-1.5 px-3 rounded-md text-xs font-medium bg-${color}-50 text-${color}-800 dark:bg-${color}-800/30 dark:text-${color}-500`}>{label}</span>
-    )
- }
- return (
-    <>
-        <span className={`inline-flex items-center gap-x-1.5 py-1.5 px-2.5  rounded-md text-xs font-medium bg-${color}-500 text-white`}>{label}</span>
-    </>
- )   
-
-}
+      <>
+        <span
+          className={`inline-flex items-center gap-x-1.5 py-1.5 px-1.5  rounded-md text-xs font-medium bg-${color}-500 text-white`}
+        >
+          {iconLeft}
+          {label}
+          {iconRight}
+        </span>
+      </>
+    );
+  };
+  
--- src/stories/Button/Button.jsx
+++ src/stories/Button/Button.jsx
@@ -2,15 +2,103 @@ import React from "react";
 import "preline/dist/preline";
 
 /** Primary UI component for user interaction */
-export const Button = ({ label, type, color, disabled }) => {
+export const Button = ({
+  label,
+  type,
+  color,
+  disabled,
+  size,
+  iconLeft,
+  iconRight,
+}) => {
+  if (size == "small") {
+    if (type == "outline") {
+      return (
+        <button
+          type="button"
+          className={`py-2.5 px-2 inline-flex items-center gap-x-2 text-xs font-semibold rounded-lg border border-gray-200 text-gray-500 hover:border-${color}-600 hover:text-${color}-600 focus:outline-none focus:border-${color}-600 focus:text-${color}-600 disabled:opacity-50 disabled:pointer-events-none dark:border-neutral-700 dark:text-neutral-400 dark:hover:text-${color}-500 dark:hover:border-${color}-600 dark:focus:text-${color}-500 dark:focus:border-${color}-600`}
+          disabled={disabled}
+        >
+          {iconLeft}
+          {label}
+          {iconRight}
+        </button>
+      );
+    }
+    if (type == "ghost") {
+      return (
+        <button
+          type="button"
+          className={`py-2.5 px-2 inline-flex items-center gap-x-2 text-xs font-semibold rounded-lg border border-transparent text-${color}-600 hover:bg-${color}-100 hover:text-${color}-800 focus:outline-none focus:bg-${color}-100 focus:text-${color}-800 disabled:opacity-50 disabled:pointer-events-none dark:text-${color}-500 dark:hover:bg-${color}-800/30 dark:hover:text-${color}-400 dark:focus:bg-${color}-800/30 dark:focus:text-${color}-400`}
+          disabled={disabled}
+        >
+          {iconLeft}
+          {label}
+          {iconRight}
+        </button>
+      );
+    }
+    if (type == "soft") {
+      return (
+        <button
+          type="button"
+          className={`py-2.5 px-2 inline-flex items-center gap-x-2 text-xs font-semibold rounded-lg border border-transparent bg-${color}-100 text-${color}-800 hover:bg-${color}-200 focus:outline-none focus:bg-${color}-200 disabled:opacity-50 disabled:pointer-events-none dark:text-${color}-400 dark:hover:bg-${color}-900 dark:focus:bg-${color}-900`}
+          disabled={disabled}
+        >
+          {iconLeft}
+          {label}
+          {iconRight}
+        </button>
+      );
+    }
+    if (type == "white") {
+      return (
+        <button
+          type="button"
+          className={`py-2.5 px-2 inline-flex items-center gap-x-2 text-xs font-semibold rounded-lg border border-gray-200 bg-white text-${color}-600 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-${color}-100 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700`}
+          disabled={disabled}
+        >
+          {iconLeft}
+          {label}
+          {iconRight}
+        </button>
+      );
+    }
+    if (type == "link") {
+      return (
+        <button
+          type="button"
+          className={`py-2.5 px-2 inline-flex items-center gap-x-2 text-xs font-semibold rounded-lg border border-transparent text-${color}-600 hover:text-${color}-800 focus:outline-none focus:text-${color}-800 disabled:opacity-50 disabled:pointer-events-none dark:text-${color}-500 dark:hover:text-${color}-400 dark:focus:text-${color}-400`}
+          disabled={disabled}
+        >
+          {iconLeft}
+          {label}
+          {iconRight}
+        </button>
+      );
+    }
+    return (
+      <button
+        type="button"
+        className={`py-2.5 px-2 inline-flex items-center gap-x-2 text-xs text-color-white font-semibold rounded-lg border border-transparent bg-${color}-500 text-white hover:bg-${color}-700 focus:outline-none focus:bg-${color}-700 disabled:opacity-50 disabled:pointer-events-none`}
+        disabled={disabled}
+      >
+        {iconLeft}
+        {label}
+        {iconRight}
+      </button>
+    );
+  }
   if (type == "outline") {
     return (
       <button
         type="button"
-        class={`py-3.5 px-4 inline-flex items-center gap-x-2 text-input font-semibold rounded-lg border border-gray-200 text-gray-500 hover:border-${color}-600 hover:text-${color}-600 focus:outline-none focus:border-${color}-600 focus:text-${color}-600 disabled:opacity-50 disabled:pointer-events-none dark:border-neutral-700 dark:text-neutral-400 dark:hover:text-${color}-500 dark:hover:border-${color}-600 dark:focus:text-${color}-500 dark:focus:border-${color}-600`}
+        className={`py-3.5 px-4 inline-flex items-center gap-x-2 text-input font-semibold rounded-lg border border-gray-200 text-gray-500 hover:border-${color}-600 hover:text-${color}-600 focus:outline-none focus:border-${color}-600 focus:text-${color}-600 disabled:opacity-50 disabled:pointer-events-none dark:border-neutral-700 dark:text-neutral-400 dark:hover:text-${color}-500 dark:hover:border-${color}-600 dark:focus:text-${color}-500 dark:focus:border-${color}-600`}
         disabled={disabled}
       >
+        {iconLeft}
         {label}
+        {iconRight}
       </button>
     );
   }
@@ -18,10 +106,12 @@ export const Button = ({ label, type, color, disabled }) => {
     return (
       <button
         type="button"
-        class={`py-3.5 px-4 inline-flex items-center gap-x-2 text-input font-semibold rounded-lg border border-transparent text-${color}-600 hover:bg-${color}-100 hover:text-${color}-800 focus:outline-none focus:bg-${color}-100 focus:text-${color}-800 disabled:opacity-50 disabled:pointer-events-none dark:text-${color}-500 dark:hover:bg-${color}-800/30 dark:hover:text-${color}-400 dark:focus:bg-${color}-800/30 dark:focus:text-${color}-400`}
+        className={`py-3.5 px-4 inline-flex items-center gap-x-2 text-input font-semibold rounded-lg border border-transparent text-${color}-600 hover:bg-${color}-100 hover:text-${color}-800 focus:outline-none focus:bg-${color}-100 focus:text-${color}-800 disabled:opacity-50 disabled:pointer-events-none dark:text-${color}-500 dark:hover:bg-${color}-800/30 dark:hover:text-${color}-400 dark:focus:bg-${color}-800/30 dark:focus:text-${color}-400`}
         disabled={disabled}
       >
+        {iconLeft}
         {label}
+        {iconRight}
       </button>
     );
   }
@@ -29,10 +119,12 @@ export const Button = ({ label, type, color, disabled }) => {
     return (
       <button
         type="button"
-        class={`py-3.5 px-4 inline-flex items-center gap-x-2 text-input font-semibold rounded-lg border border-transparent bg-${color}-100 text-${color}-800 hover:bg-${color}-200 focus:outline-none focus:bg-${color}-200 disabled:opacity-50 disabled:pointer-events-none dark:text-${color}-400 dark:hover:bg-${color}-900 dark:focus:bg-${color}-900`}
+        className={`py-3.5 px-4 inline-flex items-center gap-x-2 text-input font-semibold rounded-lg border border-transparent bg-${color}-100 text-${color}-800 hover:bg-${color}-200 focus:outline-none focus:bg-${color}-200 disabled:opacity-50 disabled:pointer-events-none dark:text-${color}-400 dark:hover:bg-${color}-900 dark:focus:bg-${color}-900`}
         disabled={disabled}
       >
+        {iconLeft}
         {label}
+        {iconRight}
       </button>
     );
   }
@@ -40,10 +132,12 @@ export const Button = ({ label, type, color, disabled }) => {
     return (
       <button
         type="button"
-        class={`py-3.5 px-4 inline-flex items-center gap-x-2 text-input font-semibold rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700`}
+        className={`py-3.5 px-4 inline-flex items-center gap-x-2 text-input font-semibold rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700`}
         disabled={disabled}
       >
+        {iconLeft}
         {label}
+        {iconRight}
       </button>
     );
   }
@@ -51,20 +145,24 @@ export const Button = ({ label, type, color, disabled }) => {
     return (
       <button
         type="button"
-        class={`py-3.5 px-4 inline-flex items-center gap-x-2 text-input font-semibold rounded-lg border border-transparent text-${color}-600 hover:text-${color}-800 focus:outline-none focus:text-${color}-800 disabled:opacity-50 disabled:pointer-events-none dark:text-${color}-500 dark:hover:text-${color}-400 dark:focus:text-${color}-400`}
+        className={`py-3.5 px-4 inline-flex items-center gap-x-2 text-input font-semibold rounded-lg border border-transparent text-${color}-600 hover:text-${color}-800 focus:outline-none focus:text-${color}-800 disabled:opacity-50 disabled:pointer-events-none dark:text-${color}-500 dark:hover:text-${color}-400 dark:focus:text-${color}-400`}
         disabled={disabled}
       >
+        {iconLeft}
         {label}
+        {iconRight}
       </button>
     );
   }
   return (
     <button
       type="button"
-      class={`py-3.5 px-4 inline-flex items-center gap-x-2 text-input text-color-white font-semibold rounded-lg border border-transparent bg-${color}-500 text-white hover:bg-${color}-700 focus:outline-none focus:bg-${color}-700 disabled:opacity-50 disabled:pointer-events-none`}
+      className={`py-3.5 px-4 inline-flex items-center gap-x-2 text-input text-color-white font-semibold rounded-lg border border-transparent bg-${color}-500 text-white hover:bg-${color}-700 focus:outline-none focus:bg-${color}-700 disabled:opacity-50 disabled:pointer-events-none`}
       disabled={disabled}
     >
+      {iconLeft}
       {label}
+      {iconRight}
     </button>
   );
 };
