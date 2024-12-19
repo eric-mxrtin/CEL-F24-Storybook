8749e771eb5e7fb67a8a42b19c33103492c1b320
Eric M <ern.martin12@gmail.com>
2024-12-19 10:31:07 -0500

    reworked radio and documented its usage

---
+++ src/stories/Radio/Radio.stories.jsx
@@ -0,0 +1,84 @@
+import { ArgTypes } from "@storybook/blocks";
+import { RadioGroup, Radio } from "./Radio";
+
+export default {
+  title: "Documented Components/Radio",
+  component: RadioGroup,
+  parameters: {
+    docs: {
+      description: {
+        component: "Radio Input Element",
+      },
+    },
+    layout: "centered",
+  },
+  argTypes: {
+    title: {
+      description: "Title for the question.",
+    },
+    options: {
+      description: "Radio options passed in as an array of strings",
+    },
+  },
+  tags: ["autodocs"],
+};
+
+export const Default = {
+  render: (args) => (
+    <RadioGroup {...args}>
+      <Radio>Option 1</Radio>
+      <Radio>Option 2</Radio>
+      <Radio>Option 3</Radio>
+    </RadioGroup>
+  ),
+  args: {
+    title: "Options"
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: "This story demonstrates the usage of a Radio group.",
+      },
+    },
+  },
+};
+
+export const Disabled = {
+  render: (args) => (
+    <RadioGroup {...args}>
+      <Radio disabled>Option 1</Radio>
+      <Radio disabled>Option 2</Radio>
+      <Radio disabled>Option 3</Radio>
+    </RadioGroup>
+  ),
+  args: {
+    title: "Options"
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: "This story demonstrates the usage of a disabled Radio group.",
+      },
+    },
+  },
+};
+
+export const Mixed = {
+  render: (args) => (
+    <RadioGroup {...args}>
+      <Radio>Option 1</Radio>
+      <Radio disabled>Option 2</Radio>
+      <Radio >Option 3</Radio>
+    </RadioGroup>
+  ),
+  args: {
+    title: "Options",
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: "This story demonstrates the usage of a mixed Radio group.",
+      },
+    },
+  },
+};
